387c31ad474adf9e27e4d04f43f256ed365efcfd
Sankalp Bhatia <sankal@amazon.com>
2018-12-11 18:56:37 +0530

    Adding a sample solution

--- src/main/java/BillingModule.java
+++ src/main/java/BillingModule.java
@@ -1,11 +1,24 @@
+import annotations.Paypal;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Names;
 
 
 public class BillingModule extends AbstractModule {
     @Override
     protected void configure() {
-        // Make changes here,
+        bind(CreditCardProcessor.class).annotatedWith(Paypal.class).to(PaypalCreditCardProcessor.class);
+        bind(CreditCardProcessor.class).annotatedWith(Names.named("Bank")).to(BankCreditCardProcessor.class);
+        bind(CreditCardProcessor.class).to(BankCreditCardProcessor.class);
+        bind(TransactionLog.class).to(InMemoryTransactionLog.class);
     }
 
     //.. or here!
+
+    @Provides
+    @Singleton
+    public BillingService getBillingService(CreditCardProcessor ccp, TransactionLog transactionLog) {
+        return new RealBillingService(ccp, transactionLog);
+    }
 }
